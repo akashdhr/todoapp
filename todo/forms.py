f0b28cc52d2bad02538adcc587b407e24801a9aa
Akash Dhar <akshdhr117@gmail.com>
2019-06-19 19:54:40 +0530

    Added list features

--- todo/forms.py
+++ todo/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class TextForm(forms.Form):
-    text = forms.CharField(label='Enter your goals for today', max_length=100)
+    text = forms.CharField(label='Enter your goals for today', max_length=100,
+    widget=forms.TextInput(attrs={ "type":"text", "name":"fname", "placeholder":"Enter your goals for today"}))
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .forms import TextForm
 from .models import Todo
 # Create your views here.
 
 def index(request):
     todo_list = Todo.objects.order_by('id')
-    context={'todo_list':todo_list}
+    todo_form = TextForm()
+    context={'todo_list':todo_list, 'todo_form': todo_form}
     return render(request, 'todo/index.html', context)
+
+def completeTodo(request, item_id):
+    todo = Todo.objects.get(pk=item_id)
+    todo.completed = True
+    todo.save()
+    return redirect('index')
