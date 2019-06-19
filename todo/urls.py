b55ccd99d71b883632f85bec9f0409855fde1afe
Akash Dhar <akshdhr117@gmail.com>
2019-06-20 00:10:53 +0530

    added forms, delete and clear all functionality

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('complete/<item_id>', views.completeTodo, name='completeitem')
+    path('complete/<item_id>', views.completeTodo, name='completeitem'),
+    path('add/', views.add_todo, name='addtodo'),
+    path('delete/', views.delete_crossed, name='delete'),
+    path('clear/', views.clear_all, name="clearall")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.views.decorators.http import require_POST
 from .forms import TextForm
 from .models import Todo
 # Create your views here.
@@ -15,3 +16,20 @@ def completeTodo(request, item_id):
     todo.completed = True
     todo.save()
     return redirect('index')
+
+def add_todo(request):
+    form = TextForm(request.POST)
+    if form.is_valid():
+        Todo(text=request.POST['text']).save()
+    return redirect('index')
+
+def delete_crossed(request):
+    Todo.objects.filter(completed=True).delete()
+    return redirect('index')
+
+def clear_all(request):
+    Todo.objects.all().delete()
+    return redirect('index')
+
+
+    
